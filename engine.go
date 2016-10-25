6c035cb530d1ac617af166df8490b3a1fad72253
Denis Vaumoron <dvaumoron@gmail.com>
2016-10-25 20:12:08 +0200

    first implementation

---
+++ engine.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"text/template"
+)
+
+func main() {
+	args := os.Args
+
+	tmplPath := args[1]
+	outPath := args[2]
+
+	tmplBody, err := ioutil.ReadFile(tmplPath)
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl, err := template.New("tmpl").Parse(tmplBody)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create(outPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	err = tmpl.Execute(file, args[3:])
+	if err != nil {
+		panic(err)
+	}
+}
