7f7ad60d2307c03a2ef23f73b196cdd14d9b1aa1
dvaumoron <dvaumoron@gmail.com>
2023-02-07 18:18:13 +0100

    use yaml for data input

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ste
+
 ste (simple template engine) is a simple wrapper to go template engine, you can use it like this :
 
-    ste templateFileName outputFileName string1 string2
+    ste templateFilePath dataFilePath outputFilePath
 
-where the template ([using go template syntax](https://golang.org/pkg/text/template/)) receive string1 as value1, string2 as value2 (and so on...), and values contains a slice with string1 and string2
+where the template ([using go template syntax](https://golang.org/pkg/text/template/)) receive data from the [yaml](https://yaml.org/) parsing of the data file.
--- engine.go
+++ engine.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"text/template"
+
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
 	args := os.Args
+	if len(args) < 4 {
+		fmt.Println("Usage : ste file.tmpl data.yaml outputFile")
+		return
+	}
 
 	tmplPath := args[1]
-	outPath := args[2]
+	dataPath := args[2]
+	outPath := args[3]
 
 	tmplBody, err := os.ReadFile(tmplPath)
 	if err != nil {
@@ -25,19 +31,25 @@ func main() {
 		return
 	}
 
+	dataBody, err := os.ReadFile(dataPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	tmplArgs := map[string]any{}
+	err = yaml.Unmarshal(dataBody, tmplArgs)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	file, err := os.Create(outPath)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer file.Close()
-
-	tmplArgs := map[string]any{}
-	values := args[3:]
-	tmplArgs["values"] = values
-	for i, value := range values {
-		tmplArgs["value"+strconv.Itoa(i+1)] = value
-	}
 
 	err = tmpl.Execute(file, tmplArgs)
 	if err != nil {
